7120d087a246b92a898b03d9491d613e8d8bb2e5
9527q <liuyu_qi617@163.com>
2023-01-03 16:42:21 +0800

    转换 API 数据的功能

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 uTools 上的自己的命令的代码
 
 * [处理批量文本](./handle_bulk_text.py)
+* [转换 API 数据](./convert_api.py)
---
+++ convert_api.py
@@ -0,0 +1,72 @@
+"""
+转换 API 数据（py>=3.9)
+
+API 文档里的实例数据一般都是 json 的，其中的注释标志是 `//`，本脚本将其转换为 `#`
+"""
+
+
+def convert(api_text: str) -> str:
+    lines = []
+    for line in api_text.split("\n"):
+        index = line.find("//")
+        if index == -1:
+            lines.append(line)
+            continue
+
+        left_str, right_str = line[:index], line[index + 2 :]
+
+        # // 开头的情况
+        if left_str.strip() == "":
+            lines.append(left_str + "# " + right_str.strip())
+            continue
+
+        # // 前面是逗号的情况
+        if left_str.rstrip()[-1] == ",":
+            lines.append(left_str.rstrip() + "  # " + right_str.strip())
+            continue
+
+        # 无法识别的情况
+        lines.append(line)
+
+    return "\n".join(lines)
+
+
+def test():
+    # 测试用例：内容、命令、结果
+    test_case: list[tuple[str, str]] = [
+        ("", ""),
+        (
+            """
+//hhh
+
+    //  hhh  
+    abc,//kk
+""",
+            """
+# hhh
+
+    # hhh
+    abc,  # kk
+""",
+        ),
+    ]
+    for input_text, result in test_case:
+        now_result = convert(input_text)
+        if now_result == result:
+            print(".", end="")
+        else:
+            print("测试失败")
+            print(f"{input_text=}")
+            print(f"{result=}")
+            print(f"{now_result=}")
+
+    print()
+
+
+def main():
+    print(convert("""{{ClipText}}"""))
+
+
+if __name__ == "__main__":
+    test()
+    # main()
